91561656a72744e80f1ffb462db007b993ebcfda
vinhvinhvroom <vinh.doan13@gmail.com>
2020-03-23 15:21:08 -0400

    added exception

--- MenuItem.cs
+++ MenuItem.cs
@@ -6,6 +6,22 @@ namespace dotnet_DinerMax3000Console
     {
         public string Title; 
         public string Description;
-        public double Price; 
+        public double Price 
+        {
+            get { return _price; } 
+            set { 
+                if (value > 0) 
+                {
+                    _price = value; 
+                }
+                else 
+                {
+                    throw new ApplicationException("You cannot have free or negative valued items.");
+                }
+             }
+        }
+
+        private double _price; 
+
     }
 }
