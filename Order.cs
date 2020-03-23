09b2f9fbc18000c8eb7d66181a7f7cb04fb322b9
vinhvinhvroom <vinh.doan13@gmail.com>
2020-03-23 15:14:02 -0400

    Added Getter to Order

--- Order.cs
+++ Order.cs
@@ -7,5 +7,18 @@ using System.Threading.Tasks;
 namespace dotnet_DinerMax3000Console {
     public class Order {
         public List <MenuItem> items = new List<MenuItem>();
+
+        public double Total 
+        {
+            get 
+            {
+                double calculatedTotal = 0; 
+                foreach(MenuItem item in items) 
+                {
+                    calculatedTotal = calculatedTotal + item.Price;
+                }
+                return calculatedTotal; 
+            }
+        }
     }
 }
